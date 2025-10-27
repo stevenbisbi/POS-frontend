1ea6da5e4c42496705ed2cf647b6e3640b82b86d
stevenbisbi <stevenex13@gmail.com>
2025-10-27 12:36:50 -0500

    api

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://pos-backend-coral-two.vercel.app";
+const API_BASE_URL = "https://pos-backend-zeta-ten.vercel.app";
 
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
