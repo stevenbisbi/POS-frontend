2b5c89ea143004d48ef5c1f5199012d1cc0f9869
stevenbisbi <stevenex13@gmail.com>
2025-10-27 13:03:09 -0500

    routes auth

--- src/services/auth.api.js
+++ src/services/auth.api.js
@@ -4,10 +4,10 @@ export const getAllUsers = () => axiosClient.get("/users");
 
 export const getUser = (id) => axiosClient.get(`/users/${id}`);
 
-export const register = (user) => axiosClient.post("/register", user);
+export const register = (user) => axiosClient.post("/auth/register", user);
 
 export const loginUser = (data) =>
-  axiosClient.post("/login", data, { withCredentials: true });
+  axiosClient.post("/auth/login", data, { withCredentials: true });
 
 export const deleteUser = (id) => axiosClient.delete(`/user/${id}`);
 
@@ -15,4 +15,4 @@ export const updateUser = (id, user) => axiosClient.put(`/user/${id}`, user);
 
 export const verifyToken = () =>
   axiosClient.get("/profile", { withCredentials: true });
-export const logout = () => axiosClient.post("/logout");
+export const logout = () => axiosClient.post("/auth/logout");
