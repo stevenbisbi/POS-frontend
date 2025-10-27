ef309e83ce459dc66e7f958df260ebfd6324ba87
stevenbisbi <stevenex13@gmail.com>
2025-10-27 12:52:42 -0500

    change in axiosClient

--- src/services/axiosClient.js
+++ src/services/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://pos-backend-zeta-ten.vercel.app";
+const API_BASE_URL = "https://pos-backend-zeta-ten.vercel.app/api/v1";
 
 const axiosClient = axios.create({
   baseURL: API_BASE_URL,
